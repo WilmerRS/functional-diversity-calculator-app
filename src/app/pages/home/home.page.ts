b00dce815dfa83ff018d563ef57ec0f39109fc1b
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-12-01 07:54:03 -0500

    feat: add inclusion resources and next page button to home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonApp, IonContent } from '@ionic/angular/standalone';
+import { IonApp, IonContent, IonImg } from '@ionic/angular/standalone';
+import { NextPageButtonComponent } from 'src/app/components/next-page-button/next-page-button.component';
 import { NotebookBackgroundComponent } from 'src/app/components/notebook-background/notebook-background.component';
 
 @Component({
@@ -7,6 +8,12 @@ import { NotebookBackgroundComponent } from 'src/app/components/notebook-backgro
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonApp, IonContent, NotebookBackgroundComponent],
+  imports: [
+    IonApp,
+    IonContent,
+    IonImg,
+    NotebookBackgroundComponent,
+    NextPageButtonComponent,
+  ],
 })
 export class HomePage {}
