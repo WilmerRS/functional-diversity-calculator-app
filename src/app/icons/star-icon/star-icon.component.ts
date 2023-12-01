54aeec0f46178e33b63fd141ac3cc31c4601f306
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-12-01 07:53:43 -0500

    feat: add StarIconComponent

---
+++ src/app/icons/star-icon/star-icon.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './star-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StarIconComponent {}
