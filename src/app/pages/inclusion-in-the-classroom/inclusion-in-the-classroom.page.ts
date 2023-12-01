8ca55ccca798396caf6e8db12da205849cd97cb3
WilmerRS <est.wilmerrodriguez@gmail.com>
2023-12-01 07:54:13 -0500

    feat: add inclusion-in-the-classroom page with necessary components

---
+++ src/app/pages/inclusion-in-the-classroom/inclusion-in-the-classroom.page.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonApp, IonContent, IonImg } from '@ionic/angular/standalone';
+import { BoardCardComponent } from 'src/app/components/board-card/board-card.component';
+import { EmailInputComponent } from 'src/app/components/email-input/email-input.component';
+import { MenuButtonComponent } from 'src/app/components/menu-button/menu-button.component';
+import { NextPageButtonComponent } from 'src/app/components/next-page-button/next-page-button.component';
+import { NotebookBackgroundComponent } from 'src/app/components/notebook-background/notebook-background.component';
+import { UserStateService } from 'src/app/services/user-state.service';
+
+@Component({
+  selector: 'app-inclusion-in-the-classroom',
+  templateUrl: 'inclusion-in-the-classroom.page.html',
+  styleUrls: ['inclusion-in-the-classroom.page.scss'],
+  standalone: true,
+  imports: [
+    IonApp,
+    IonContent,
+    IonImg,
+    NotebookBackgroundComponent,
+    NextPageButtonComponent,
+    BoardCardComponent,
+    MenuButtonComponent,
+    EmailInputComponent,
+    ReactiveFormsModule,
+  ],
+})
+export class InclusionInTheClassroomPage {
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+
+  private _userStateService = inject(UserStateService);
+
+  constructor() {
+    this.emailControl.setValue(this._userStateService.email);
+  }
+
+  saveEmailUser() {
+    if (!this.emailControl.value) {
+      return;
+    }
+
+    this._userStateService.saveEmailUser(this.emailControl.value);
+  }
+}
